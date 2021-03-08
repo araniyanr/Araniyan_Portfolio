aff0f057319f67ec773870db3e09368466e9f0de
Abelaash Giritharan <50346809+Abelaash@users.noreply.github.com>
2021-03-08 15:04:43 -0500

    initial commit

---
+++ button.js
@@ -0,0 +1,7 @@
+function button(project, change){
+    var projOne = document.getElementById("projOne");
+    projOne.style.display = "none";
+
+
+    document.getElementById(project).style.display = "block";
+}
---
+++ typeit.js
@@ -0,0 +1,7 @@
+// From typeit.com
+
+var type = new TypeIt('#info',{
+    strings: ['This is my string!'],
+    
+})
+
